539d9d1ffeeeba411fe103473987c1c87d1484db
FAMILIAR project <mathieu.acher@irisa.fr>
2020-01-21 14:54:12 +0100

    Update app.js

--- tooling/wordcloudgenerator/app.js
+++ tooling/wordcloudgenerator/app.js
@@ -53,7 +53,7 @@ function getWords(words) {
     return res;
 }
 
-https.get('https://haltools.inria.fr/Public/exportPubli.php?format_export=bibtex&annee_publideb=2016&annee_publifin=2018&labos_exp=diverse;triskell&CB_auteur=oui&CB_aff_abstract=oui&CB_titre=oui&CB_article=oui&langue=Anglais&tri_exp=typdoc&tri_exp2=auteur_exp&tri_exp3=titre&ordre_aff=TA', (res) => {
+https.get('https://haltools.inria.fr/Public/exportPubli.php?format_export=bibtex&annee_publideb=2016&annee_publifin=2020&labos_exp=diverse;triskell&CB_auteur=oui&CB_aff_abstract=oui&CB_titre=oui&CB_article=oui&langue=Anglais&tri_exp=typdoc&tri_exp2=auteur_exp&tri_exp3=titre&ordre_aff=TA', (res) => {
     const {
         statusCode
     } = res;
