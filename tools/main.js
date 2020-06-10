63f72ff74b670c8d9e3d22d422eeef834651eee2
oscarlvp <oscarlvp@gmail.com>
2020-06-10 13:12:06 +0200

    Missing main.js

---
+++ tools/main.js
@@ -0,0 +1,25 @@
+const colors = require('colors');
+const updatebib = require('./updatebibterms.js');
+const updatevideos = require('./fetchvideos.js');
+const fetchfeed = require('./fetchposts.js');
+const updateimages = require('./fetchpics.js');
+
+
+async function main() {
+
+    const steps = [
+        { title: "Updating bibliography items", command: updatebib    },
+        { title: "Updating blog posts",         command: fetchfeed    },
+        { title: "Updating YouTube videos",     command: updatevideos },
+        { title: "Updating profile images",     command: updateimages }
+    ];
+
+    for (let step of steps) {
+        console.log(step.title.green);
+        await  step.command.call();
+        console.log('Done\n'.green);
+    }
+
+}
+
+main();
